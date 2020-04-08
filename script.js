0cc3e2467c07d6eb6065ed036c4cf864966b352c
Nino Chimento <saninoc84@gmail.com>
2020-04-08 14:56:27 +0200

    elimino elemento

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'Todo Vuejs!'
+        title: 'Todo Vuejs!',
+        todos :[
+            {
+                text : "imparare VueJs",
+                check : false
+            },
+            {
+                text : "trasferirmi",
+                check :false
+            }
+    ],
+    },
+    methods:{
+        cancella(index){
+            this.todos.splice(index,1);
+        }
     }
 })
+            
+            
